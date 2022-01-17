99656783d8771d85e8c8bfd4309ff6fb217a4011
Srilekha Kadambala <71275235+srilekhaK9120@users.noreply.github.com>
2022-01-17 23:33:34 +0530

    Add files via upload

---
+++ Intermediate Code Generation/Test Cases/test-3.c
@@ -0,0 +1,15 @@
+//Testing scope
+int main(void)
+{
+     int a;
+     int b;
+     b=100;
+     a=200;
+     if(b>a) {
+     	int a = 5;
+	b=b+a;
+    }
+    else{
+	b=b+a;
+    }
+}
---
+++ Intermediate Code Generation/Test Cases/test-4.c
@@ -0,0 +1,13 @@
+//Testing static type checking.
+
+int main()
+{
+    int a, b, c;
+     float d, e, f;
+    a=10;
+    b=20;
+     c=30;
+     f=2.3;
+     d = c+f;
+     e=f+f;
+}
